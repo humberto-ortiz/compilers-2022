88334d420d407c8137768d8e790e3da93cb7afa9
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2022-02-17 11:25:28 -0400

    Extend the compiler to support add1 and sub1

--- dpc/compiler.ml
+++ dpc/compiler.ml
@@ -1,6 +1,15 @@
 open Printf
 
-type expr = int64
+type expr = 
+  | Num of int64
+  | Add1 of expr
+  | Sub1 of expr
+
+let rec interp_expr (e : expr) : int64 =
+  match e with
+  | Num n -> n
+  | Add1 ea -> Int64.add 1L (interp_expr ea)
+  | Sub1 es -> Int64.add (-1L) (interp_expr es)
 
 type reg = 
   | RAX
@@ -11,6 +20,8 @@ type arg =
 
 type instruction = 
   | IMov of arg * arg
+  | IAdd of arg * arg
+  | ISub of arg * arg
 
 let reg_to_string r =
   match r with
@@ -24,26 +35,28 @@ let arg_to_string (arg) : string =
 let inst_to_string (inst : instruction) : string =
   match inst with
   | IMov (a, b) -> "mov " ^ (arg_to_string a) ^ ", " ^
-                     (arg_to_string b) ^ "\n"
+                     (arg_to_string b)
+  | IAdd (a, b) -> "add " ^ (arg_to_string a) ^ ", " ^
+                     (arg_to_string b)
+  | ISub (a, b) -> "sub " ^ (arg_to_string a) ^ ", " ^
+                     (arg_to_string b)
 
 let rec asm_to_string (asm : instruction list) : string =
-  match asm with
-  | [] -> ""
-  | (inst::instrs) -> (inst_to_string inst) ^ (asm_to_string instrs)
+  String.concat "\n\t" (List.map inst_to_string asm)
 
+let rec compile_expr (e : expr) : instruction list =
+  match e with
+  | Num n -> [IMov (Reg RAX, Constant n)]
+  | Add1 ea -> (compile_expr ea) @ [IAdd (Reg RAX, Constant 1L)]
+  | Sub1 es -> (compile_expr es) @ [ISub (Reg RAX, Constant 1L)]
+;;
 
-let compile_expr (e : expr) : instruction list =
-  [IMov (Reg RAX, Constant e)];;
-
- 
 let compile_prog (e : expr) : string =
   let prog_string =  asm_to_string (compile_expr e) in
-  sprintf "
-section .text
+  sprintf "section .text
 global our_code_starts_here
 our_code_starts_here:
-" ^ prog_string ^ "
-
+\t" ^ prog_string ^ "
         ret\n" 
 
 (* Some OCaml boilerplate for reading files and command-line arguments *)
