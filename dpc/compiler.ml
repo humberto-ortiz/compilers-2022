0dddda15fdc59c3b3954cbf04d61a5f9be9e7ade
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2022-03-03 11:43:29 -0400

    Break compiler. Implement half of let expressions.

--- dpc/compiler.ml
+++ dpc/compiler.ml
@@ -3,10 +3,12 @@ open Syntax
 
 type reg = 
   | RAX
+  | RSP
 
 type arg =
   | Constant of int64
   | Reg of reg
+  | RegOffset of reg * int64
 
 type instruction = 
   | IMov of arg * arg
@@ -16,11 +18,14 @@ type instruction =
 let reg_to_string r =
   match r with
   | RAX -> "RAX"
+  | RSP -> "RSP"
 
 let arg_to_string (arg) : string =
   match arg with
   | Constant n -> Int64.to_string n
   | Reg r -> reg_to_string r
+  | RegOffset (r, n) -> 
+     sprintf "[%s - 8* %Ld]" (reg_to_string r) (n)
 
 let inst_to_string (inst : instruction) : string =
   match inst with
@@ -34,11 +39,24 @@ let inst_to_string (inst : instruction) : string =
 let asm_to_string (asm : instruction list) : string =
   String.concat "\n\t" (List.map inst_to_string asm)
 
-let rec compile_expr (e : expr) : instruction list =
+type env = (string * int64) list
+
+let rec lookup id env =
+  match env with
+    | [] -> failwith (sprintf "No se encontro %s" id)
+    | (v, slot)::rest -> if id = v then slot else lookup id rest
+
+let rec compile_expr (e : expr) (env : env) : instruction list =
   match e with
   | Num n -> [IMov (Reg RAX, Constant n)]
-  | Add1 ea -> (compile_expr ea) @ [IAdd (Reg RAX, Constant 1L)]
-  | Sub1 es -> (compile_expr es) @ [ISub (Reg RAX, Constant 1L)]
+  | Add1 ea -> (compile_expr ea env) @ [IAdd (Reg RAX, Constant 1L)]
+  | Sub1 es -> (compile_expr es env) @ [ISub (Reg RAX, Constant 1L)]
+  | Id id ->
+     [ IMov (Reg RAX, RegOffset (RSP, lookup id env)) ]
+  | Let (id, e1, e2) ->
+     let (env', slot) = update id env in
+     compile_expr e1 env
+     @ [ IMov (RegOffset (RSP, slot), Reg RAX) ]
   | _ -> failwith "No se compilar eso todavia"
 ;;
 
